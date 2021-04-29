eb08baa40c2fbeb74ac75157f2fe1a4c430c9ff0
AleksandrPalenko <palenko.as@mail.ru>
2021-04-29 16:54:55 +0300

    3. Перегрузить метод max для трех чисел [#496291]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -2,13 +2,18 @@ package ru.job4j.oop;
 
 public class Calculator {
 
-    public int max(int first, int second) {
+    public static int max(int first, int second) {
         return first > second ? first : second;
 
     }
 
-    public int max(int first, int second, int third) {
-        return max(this.max(first, second), third);
+    public static int max(int first, int second, int third) {
+        int tmp = max(first, second);
+        return max(tmp, third);
 
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(max(first, second, third), four);
     }
 }
--- src/test/java/ru/job4j/oop/CalculatorTest.java
+++ src/test/java/ru/job4j/oop/CalculatorTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.oop;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,15 +8,13 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
 
     @Test
-    public void max() {
-
-    }
-
-    @Test
     public void testMax() {
         int first = 1;
         int second = 2;
         int third = 3;
-        Calculator.max
+        int four = 4;
+        int result = Calculator.max(first, second, third, four);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
     }
 }
