228afdd50e40c0bd695748ec87646015bc760f5a
AleksandrPalenko <palenko.as@mail.ru>
2021-05-04 16:09:08 +0300

    3. Массивы и модели. [#496312]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int countOfPage;
+
+    public Book(String name, int countOfPage) {
+        this.name = name;
+        this.countOfPage = countOfPage;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCountOfPage() {
+        return countOfPage;
+    }
+
+    public void setCountOfPage(int countOfPage) {
+        this.countOfPage = countOfPage;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+public class Library extends Book {
+    public Library(String name, int countOfPage) {
+        super(name, countOfPage);
+    }
+
+    public static void main(String[] args) {
+        Book cleanCode = new Book("cleanCode", 300);
+        Book javaRush = new Book("javaRush", 200);
+        Book job4j = new Book("job4j", 120);
+        Book geekBrains = new Book("geekBrains", 250);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = javaRush;
+        books[2] = job4j;
+        books[3] = geekBrains;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " = " + bk.getCountOfPage());
+        }
+        System.out.println(" swap 0 and 3 ");
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            Book tmp = books[0];
+            books[0] = books[3];
+            books[3] = tmp;
+            System.out.println(bk.getName() + " = " + bk.getCountOfPage());
+        }
+        System.out.println(" Just book where name like 'clean code'");
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.getName() == "cleanCode") {
+                System.out.println(bk.getName());
+            }
+        }
+    }
+}
