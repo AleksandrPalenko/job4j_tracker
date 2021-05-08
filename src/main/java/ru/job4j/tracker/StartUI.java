0a73eb3dd3f1b2805d22b69b2c344aed218ca9d2
AleksandrPalenko <palenko.as@mail.ru>
2021-05-08 20:38:38 +0300

    9. toString [#496319]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,14 +9,19 @@ public class StartUI {
         Tracker track = new Tracker();
         Item item = new Item();
         Item item1 = new Item();
+        Item item2 = new Item();
         item.setId(1);
         item.setName("первый");
         item1.setId(2);
         item1.setName("номер");
+        item2.setId(3);
+        item2.setName("Второй");
         track.add(item);
         track.add(item1);
+        track.add(item2);
         System.out.println(track.findById(1));
         System.out.println(track.findById(2));
+        System.out.println(track.findById(3));
     }
 }
 
