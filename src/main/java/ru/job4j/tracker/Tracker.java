995b7370796df0e60bb9cac3d1c14ada2c6aac17
AleksandrPalenko <palenko.as@mail.ru>
2021-05-12 20:19:28 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #200547]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -64,7 +64,7 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        System.arraycopy(items, index + 1, items, index, this.size - index);
+        System.arraycopy(items, index + 1, items, index, this.size - 1);
         items[size - 1] = null;
         size--;
         return true;
