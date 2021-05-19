cbb0ece1cbce1b1f1f44a16eea3fea6fa73d30c3
AleksandrPalenko <palenko.as@mail.ru>
2021-05-19 12:34:24 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -6,12 +6,12 @@ public class Fact {
     }
 
     public int calc(int n) {
-        int rsl = -1;
-        for (int i = 1; i <= n; i++) {
-            if (n < 0) {
-                throw new IllegalArgumentException("the input element must be greater than zero");
-            }
-            rsl = i * i;
+        if (n < 0) {
+            throw new IllegalArgumentException("N could not be less then 0");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
         }
         return rsl;
     }
