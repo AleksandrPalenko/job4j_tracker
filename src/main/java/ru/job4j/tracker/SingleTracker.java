ec31cad2d22e0b8f5e711a18c7513fb955f7540d
AleksandrPalenko <palenko.as@mail.ru>
2021-05-24 13:59:14 +0300

    2. Singleton [#94606 #159176]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,10 +2,14 @@ package ru.job4j.tracker;
 
 public class SingleTracker {
     private Tracker tracker = new Tracker();
+    private static SingleTracker instance = null;
+    private int id = 0;
+    private Item[] items = new Item[100];
 
     private SingleTracker() {
 
     }
+
 
     public Item add(Item item) {
         return tracker.add(item);
@@ -25,5 +29,12 @@ public class SingleTracker {
 
     public boolean delete(int id) {
         return tracker.delete(id);
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 }
