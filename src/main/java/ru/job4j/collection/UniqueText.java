b7a4d29db6b13e0a0ba7cf058517e30792c378d0
AleksandrPalenko <palenko.as@mail.ru>
2021-06-01 16:40:25 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        /* for-each origin -> new HashSet. */
+        for (String value : origin) {
+            check.add(value);
+        }
+        /* for-each text -> hashSet.contains */
+        for (String values : text) {
+            if (!check.contains(values)) {
+                return false;
+            }
+        return rsl;
+    }
+}
