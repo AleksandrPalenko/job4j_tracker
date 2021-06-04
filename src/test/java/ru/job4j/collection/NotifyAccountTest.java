f07a6c1db5da5235f1bdd4a341ce4921127d4472
AleksandrPalenko <palenko.as@mail.ru>
2021-06-04 12:30:33 +0300

    3. Модели данных и HashSet. [#218530 #164478]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -36,7 +36,7 @@ public class NotifyAccountTest {
                         new Account("100", "Bon Jovy", "fDqwe123q")
                 )
         );
-
+        assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
 }
