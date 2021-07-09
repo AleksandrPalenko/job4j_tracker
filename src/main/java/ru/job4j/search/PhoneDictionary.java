091c20a82fd11e024ec8fc4dfac5a4323680f392
AleksandrPalenko <palenko.as@mail.ru>
2021-07-09 15:14:45 +0300

    6. Функции высшего порядка [#24255 #175234]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -14,7 +14,8 @@ public class PhoneDictionary {
         Predicate<Person> predName = (values) -> values.getName().contains(key);
         Predicate<Person> predPhone = (values) -> values.getPhone().contains(key);
         Predicate<Person> predAddress = (values) -> values.getAddress().contains(key);
-        Predicate<Person> combine = predName.or(predPhone).or(predAddress);
+        Predicate<Person> predSurname = (values) -> values.getSurname().contains(key);
+        Predicate<Person> combine = predName.or(predPhone).or(predAddress).or(predSurname);
         ArrayList<Person> result = new ArrayList<>();
         for (Person persons : person) {
             if (combine.test(persons)) {
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -13,7 +13,7 @@ public class PhoneDictionaryTest {
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
+                new Person("Petr", "Arsentev", "534871", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
