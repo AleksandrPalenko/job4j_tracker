de98fc5163f20fe53ce239525b625d787fb84385
Александр <palenko.as@mail.ru>
2021-12-25 11:15:14 +0300

    1.javadoc

--- src/main/java/ru/job4j/collection/JobAscByName.java
+++ src/main/java/ru/job4j/collection/JobAscByName.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class JobAscByName implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
-        return o2.getName().compareTo(o1.getName()); // по имени и по убыванию
+        return o2.getName().compareTo(o1.getName()); /* по имени и по убыванию*/
     }
 }
